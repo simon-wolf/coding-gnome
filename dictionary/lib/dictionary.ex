f32d5bbcce3b0c8647cec2bc8976a15edc5436f2
Simon Wolf <simon@simonwolf.io>
2018-04-14 19:51:57 +0100

    Refactored Dictionary

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,18 +1,8 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for Dictionary.
-  """
 
-  def random_word() do
-    word_list()
-    |> Enum.random()
-  end
+  alias Dictionary.WordList
 
-  def word_list do
-    "../assets/words.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> String.split(~r/\n/)
-  end
+  defdelegate start(), to: WordList, as: :word_list
+  defdelegate random_word(word_list), to: WordList
 
 end
