021e6370d3322056c671c5209f73f1de114d7801
Simon Wolf <simon@simonwolf.io>
2018-04-14 11:48:46 +0100

    Added the Dictionary.

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,18 @@
+defmodule Dictionary do
+  @moduledoc """
+  Documentation for Dictionary.
+  """
+
+  def random_word() do
+    word_list()
+    |> Enum.random()
+  end
+
+  def word_list do
+    "../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+end
